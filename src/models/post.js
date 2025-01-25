138a1811b0caf90f5deeca8b751139ee275f998c
Chris Apps <chrisapps@Chriss-Mac-mini.local>
2025-01-25 15:36:42 +1000

    Created foreign key in posts for category id

--- src/models/post.js
+++ src/models/post.js
@@ -3,7 +3,11 @@ const mongoose = require("mongoose")
 const PostSchema = mongoose.Schema({
     title: String,
     body: String,
-    is_published: Boolean
+    is_published: Boolean,
+    category_id: {
+        type: mongoose.Types.ObjectId,
+        ref: "Category" // this name must match the name of the model in category
+    }
 })
 
 const Post = mongoose.model("Post", PostSchema)
